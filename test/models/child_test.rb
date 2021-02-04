25e572c7c29ee3f9b1b910c846b0905ab15ed7bd
Eshita Banerjee <ebanerje@andrew.cmu.edu>
2021-02-04 13:49:35 -0500

    added test case for points earned

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -32,6 +32,13 @@ class ChildTest < ActiveSupport::TestCase
    should "have a scope to select only active children" do
      assert_equal ["Alex", "Mark"], Child.active.alphabetical.map{|c| c.first_name}
    end
- end
+
+   should "show the correct points earned by children" do 
+    assert_equal 4, @alex.points_earned
+    assert_equal 1, @mark.points_earned
+    assert_equal 0, @rachel.points_earned
+  end
+
+end
 
 end
