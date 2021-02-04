7eab675acee41de7b9a8584bb5a46fd8fb28c167
Eshita Banerjee <ebanerje@andrew.cmu.edu>
2021-02-04 13:19:58 -0500

    finished part 3

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,24 @@
 class Chore < ApplicationRecord
 	belongs_to :child
 	belongs_to :task
+
+	#Validations
+	validates_date :due_on
+
+	#Scope
+	scope :by_task, -> { joins(:task).order('name') }
+	scope :chronological, -> { order(:due_on, :completed)}
+	scope :alphabetical,    -> { order('name') }
+	scope :pending,         -> {where('completed = ?', false)}
+	scope :done,            -> {where('completed = ?', true)}
+	scope :upcoming, -> {where("due_on >= ?", Date.today)}
+	scope :past, -> {where("due_on < ?", Date.today)}
+
+	def status
+	if completed == true 
+		status = "Completed"
+	else status = "Pending"
+	end
+end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+	#Relationships
+	has_many :chores
+	has_many :children, through: :chores
+
+	#Validates
+	validates_presence_of :name
+	validates_numericality_of :points, :only_integer => true, :greater_than => 0
+
+	#Scopes
+	scope :alphabetical, -> { order('name') }
+	scope :active, -> { where(active: true) }
 end
