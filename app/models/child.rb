d6584514a248eae0272f79d78e12b9782fbd1ade
Eshita Banerjee <ebanerje@andrew.cmu.edu>
2021-02-04 12:56:21 -0500

    added relationships and validations to child

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,11 @@
 class Child < ApplicationRecord
+
+	#Relationships
+	has_many :chores
+	has_many :tasks, through: :chores
+
+	#Validations
+  	validates_presence_of :first_name
+  	validates_presence_of :last_name
     
 end
